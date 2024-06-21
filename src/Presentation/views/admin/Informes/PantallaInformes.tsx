b58996b47ca770b9f7e7f277474fef23ab301072
Jhonar Ariza <prueba@gmail.com>
2024-06-20 22:55:50 -0500

    getAll excel

--- src/Presentation/views/admin/Informes/PantallaInformes.tsx
+++ src/Presentation/views/admin/Informes/PantallaInformes.tsx
@@ -17,195 +17,44 @@ export default function AdminScreenInformes() {
   )
 }
 
-///
+  */
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { Button, Platform, StyleSheet, Text, View } from "react-native";
 import * as FileSystem from "expo-file-system";
 import * as Sharing from "expo-sharing";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as XLSX from "xlsx";
+import { Registro } from "../../../../Domain/entities/Registro";
+import { RegistroRepositoryImpl } from "../../../../Data/repositories/RegistroRepository";
 
-export default function App() {
-  const [downloadProgress, setDownloadProgress] = useState(0);
-  const [download, setDownload] = useState();
-  const [isDownloading, setIsDownloading] = useState(false);
-  const [isDownloaded, setIsDownloaded] = useState(false);
-  const [isPaused, setIsPaused] = useState(false);
-
-  const callback = (progress:any) => {
-    const percentProgress = (
-      (progress.totalBytesWritten / progress.totalBytesExpectedToWrite) *
-      100
-    ).toFixed(2);
-    setDownloadProgress(percentProgress);
-  };
-
-  useEffect(() => {
-    const getDownloadable = async () => {
-      try {
-        const savedDownloadJSON = await AsyncStorage.getItem("download");
-
-        if (savedDownloadJSON !== undefined && savedDownloadJSON !== null) {
-          const savedDownload = JSON.parse(savedDownloadJSON);
-          const downloadResumable = FileSystem.createDownloadResumable(
-            savedDownload.url,
-            savedDownload.fileUri,
-            savedDownload.options,
-            callback,
-            savedDownload.resumeData
-          );
-
-          setDownload(downloadResumable);
-          setIsPaused(true);
-          setIsDownloading(true);
-        } else {
-          const downloadResumable = FileSystem.createDownloadResumable(
-            "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-            FileSystem.documentDirectory + "large.mp4",
-            {},
-            callback
-          );
-          setDownload(downloadResumable);
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    getDownloadable();
-
-    return async () => {
-      if (isDownloading) {
-        await pauseDownload();
-      }
-    };
-  }, []);
-
-  const downloadFile = async () => {
-    setIsDownloading(true);
-    const { uri } = await download.downloadAsync();
-    AsyncStorage.removeItem("download");
-    setIsDownloaded(true);
-  };
-
-  const pauseDownload = async () => {
-    setIsPaused(true);
-    await download.pauseAsync();
-    AsyncStorage.setItem("download", JSON.stringify(download.savable()));
-    console.log("Paused download");
-  };
-
-  const resumeDownload = async () => {
-    setIsPaused(false);
-    const { uri } = await download.resumeAsync();
-    AsyncStorage.removeItem("download");
-    setIsDownloaded(true);
-  };
-
-  const resetDownload = async () => {
-    setIsDownloaded(false);
-    setIsDownloading(false);
-    setIsPaused(false);
-    setDownloadProgress(0);
-
-    AsyncStorage.removeItem("download");
-    const downloadResumable = FileSystem.createDownloadResumable(
-      "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-      FileSystem.documentDirectory + "large.mp4",
-      {},
-      callback
-    );
-    setDownload(downloadResumable);
-  };
-
-  const exportDownload = async () => {
-    if (Platform.OS === "android") {
-      const permissions =
-        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-      if (permissions.granted) {
-        const base64 = await FileSystem.readAsStringAsync(
-          FileSystem.documentDirectory + "large.mp4",
-          { encoding: FileSystem.EncodingType.Base64 }
-        );
-
-        await FileSystem.StorageAccessFramework.createFileAsync(
-          permissions.directoryUri,
-          "large.mp4",
-          "application/mp4"
-        )
-          .then(async (uri) => {
-            await FileSystem.writeAsStringAsync(uri, base64, {
-              encoding: FileSystem.EncodingType.base64,
-            });
-          })
-          .catch((e) => console.log(e));
-      }
-    } else {
-      await Sharing.shareAsync(FileSystem.documentDirectory + "large.mp4");
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      {isDownloading && <Text>Progress: {downloadProgress}%</Text>}
-      {!isDownloading && !isPaused && (
-        <Button title="Download" onPress={downloadFile} />
-      )}
-      {isDownloading && !isPaused && (
-        <Button title="Pause" onPress={pauseDownload} />
-      )}
-      {isPaused && <Button title="Resume" onPress={resumeDownload} />}
-      {(isDownloading || isDownloaded) && (
-        <Button title="Reset" onPress={resetDownload} />
-      )}
-
-      {isDownloaded && <Button title="Export File" onPress={exportDownload} />}
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-
-
-///
- */
-
-
-//////// inicio 
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { Button, Platform, StyleSheet, Text, View } from "react-native";
-import * as FileSystem from "expo-file-system";
-import * as Sharing from "expo-sharing";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-export default function App() {
-  const [downloadProgress, setDownloadProgress] = useState<number>(0);
-  const [download, setDownload] = useState<FileSystem.DownloadResumable | undefined>(undefined);
+export default function AdminScreenInformes() {
+  const [descargaProgreso, setDescargaProgreso] = useState<number>(0);
+  const [descarga, setDownload] = useState<FileSystem.DownloadResumable | undefined>(undefined);
   const [isDownloading, setIsDownloading] = useState<boolean>(false);
   const [isDownloaded, setIsDownloaded] = useState<boolean>(false);
   const [isPaused, setIsPaused] = useState<boolean>(false);
+
+  const [registros, setRegistros] = useState<Registro[]>([]);
 
   const callback = (progress: any) => {
     const percentProgress = (
       (progress.totalBytesWritten / progress.totalBytesExpectedToWrite) *
       100
     ).toFixed(2);
-    setDownloadProgress(Number(percentProgress));
+    setDescargaProgreso(Number(percentProgress));
   };
 
   useEffect(() => {
+    const fetchRegistros = async () => {
+      const registroRepository = new RegistroRepositoryImpl();
+      const registros = await registroRepository.getAll();
+      setRegistros(registros);
+    };
+
     const getDownloadable = async () => {
       try {
-        const savedDownloadJSON = await AsyncStorage.getItem("download");
+        const savedDownloadJSON = await AsyncStorage.getItem("descarga");
 
         if (savedDownloadJSON !== undefined && savedDownloadJSON !== null) {
           const savedDownload = JSON.parse(savedDownloadJSON);
@@ -233,6 +82,8 @@ export default function App() {
         console.log(e);
       }
     };
+
+    fetchRegistros();
     getDownloadable();
 
     return () => {
@@ -244,28 +95,32 @@ export default function App() {
 
   const downloadFile = async () => {
     setIsDownloading(true);
-    if (download) {
-      const { uri } = await download.downloadAsync();
-      AsyncStorage.removeItem("download");
-      setIsDownloaded(true);
+    if (descarga) {
+      const result = await descarga.downloadAsync();
+      if (result) {
+        AsyncStorage.removeItem("descarga");
+        setIsDownloaded(true);
+      }
     }
   };
 
   const pauseDownload = async () => {
-    if (download) {
+    if (descarga) {
       setIsPaused(true);
-      await download.pauseAsync();
-      AsyncStorage.setItem("download", JSON.stringify(download.savable()));
-      console.log("Paused download");
+      await descarga.pauseAsync();
+      AsyncStorage.setItem("descarga", JSON.stringify(descarga.savable()));
+      console.log("Paused descarga");
     }
   };
 
   const resumeDownload = async () => {
-    if (download) {
+    if (descarga) {
       setIsPaused(false);
-      const { uri } = await download.resumeAsync();
-      AsyncStorage.removeItem("download");
-      setIsDownloaded(true);
+      const result = await descarga.resumeAsync();
+      if (result) {
+        AsyncStorage.removeItem("descarga");
+        setIsDownloaded(true);
+      }
     }
   };
 
@@ -273,12 +128,12 @@ export default function App() {
     setIsDownloaded(false);
     setIsDownloading(false);
     setIsPaused(false);
-    setDownloadProgress(0);
+    setDescargaProgreso(0);
 
-    AsyncStorage.removeItem("download");
+    AsyncStorage.removeItem("descarga");
     const downloadResumable = FileSystem.createDownloadResumable(
       "https://docs.google.com/spreadsheets/d/1YiPOVlcAZzUmIObubvGVsyty2QYIMnsD/edit?usp=sharing&ouid=112342324574499616111&rtpof=true&sd=true",
-       FileSystem.documentDirectory + "prueba.xlsx",
+      FileSystem.documentDirectory + "prueba.xlsx",
       {},
       callback
     );
@@ -286,47 +141,47 @@ export default function App() {
   };
 
   const exportDownload = async () => {
-    if (Platform.OS === "android") {
-      const permissions =
-        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-      if (permissions.granted) {
-        const base64 = await FileSystem.readAsStringAsync(
-          FileSystem.documentDirectory + "prueba.xlsx",
-          { encoding: FileSystem.EncodingType.Base64 }
-        );
+    const wb = XLSX.utils.book_new();
+    const ws = XLSX.utils.json_to_sheet(registros);
+    XLSX.utils.book_append_sheet(wb, ws, "Registros");
 
+    const wbout = XLSX.write(wb, { type: "base64", bookType: "xlsx" });
+
+    const uri = FileSystem.documentDirectory + "registros.xlsx";
+    await FileSystem.writeAsStringAsync(uri, wbout, { encoding: FileSystem.EncodingType.Base64 });
+
+    if (Platform.OS === "android") {
+      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (permissions.granted) {
         await FileSystem.StorageAccessFramework.createFileAsync(
           permissions.directoryUri,
-          "prueba.xlsx",
-          "application/mp4"
+          "registros.xlsx",
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
           .then(async (uri) => {
-            await FileSystem.writeAsStringAsync(uri, base64, {
-              encoding: FileSystem.EncodingType.Base64,
-            });
+            await FileSystem.writeAsStringAsync(uri, wbout, { encoding: FileSystem.EncodingType.Base64 });
           })
           .catch((e) => console.log(e));
       }
     } else {
-      await Sharing.shareAsync(FileSystem.documentDirectory + "prueba.xlsx");
+      await Sharing.shareAsync(uri);
     }
   };
 
   return (
     <View style={styles.container}>
-      {isDownloading && <Text>Progress: {downloadProgress}%</Text>}
+      {isDownloading && <Text>Progress: {descargaProgreso}%</Text>}
       {!isDownloading && !isPaused && (
-        <Button title="Download" onPress={downloadFile} />
+        <Button title="Descargar Excel" onPress={downloadFile} />
       )}
       {isDownloading && !isPaused && (
-        <Button title="Pause" onPress={pauseDownload} />
+        <Button title="Pausar Descarga" onPress={pauseDownload} />
       )}
-      {isPaused && <Button title="Resume" onPress={resumeDownload} />}
+      {isPaused && <Button title="Reanudar Descarga" onPress={resumeDownload} />}
       {(isDownloading || isDownloaded) && (
-        <Button title="Reset" onPress={resetDownload} />
+        <Button title="Reiniciar" onPress={resetDownload} />
       )}
-
-      {isDownloaded && <Button title="Export File" onPress={exportDownload} />}
+      {isDownloaded && <Button title="Exportar Excel" onPress={exportDownload} />}
       <StatusBar style="auto" />
     </View>
   );
@@ -340,70 +195,3 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-
-
-///fin
-
-/* import React from 'react';
-import { Button, View } from 'react-native';
-import ExcelJS from 'exceljs';
-import RNFS from 'react-native-fs';
-import Share from 'react-native-share';
-import { Platform } from 'react-native';
-
-const GenerateExcel = () => {
-  const generateExcel = async () => {
-    // Crear un nuevo libro de trabajo y una hoja
-    const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('Sheet1');
-
-    // Añadir datos de ejemplo
-    worksheet.columns = [
-      { header: 'Nombre', key: 'name', width: 20 },
-      { header: 'Edad', key: 'age', width: 10 },
-    ];
-
-    worksheet.addRows([
-      { name: 'Juan', age: 28 },
-      { name: 'Ana', age: 22 },
-      { name: 'Luis', age: 32 },
-    ]);
-
-    // Escribir el archivo a un buffer
-    const buffer = await workbook.xlsx.writeBuffer();
-
-    // Convertir el buffer a base64
-    const base64 = buffer.toString('base64');
-
-    // Definir la ruta y el nombre del archivo
-    const path = `${RNFS.DocumentDirectoryPath}/example.xlsx`;
-
-    // Guardar el archivo usando RNFS
-    await RNFS.writeFile(path, base64, 'base64');
-
-    // Compartir el archivo
-    const shareOptions = {
-      url: Platform.OS === 'ios' ? path : `file://${path}`,
-      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-      title: 'Compartir archivo Excel',
-    };
-
-    try {
-      await Share.open(shareOptions);
-    } catch (error) {
-      console.error('Error al compartir el archivo', error);
-    }
-  };
-
-  return (
-    <View style={{ margin: 20 }}>
-      <Button title="Generar Excel" onPress={generateExcel} />
-    </View>
-  );
-};
-
-export default GenerateExcel;
-
-
- */
- 
